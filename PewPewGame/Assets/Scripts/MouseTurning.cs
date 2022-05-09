b2a78b5ad585c56d92fead097e575362a3804fc1
Irwan Clark <51466405+irween@users.noreply.github.com>
2022-05-09 17:51:22 +1200

    Update MouseTurning.cs

--- PewPewGame/Assets/Scripts/MouseTurning.cs
+++ PewPewGame/Assets/Scripts/MouseTurning.cs
@@ -14,7 +14,8 @@ public class MouseTurning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // points the player object towards the mouse pointer - adapted from unity forum
+        // points the player object towards the mouse pointer - adapted from unity forum :
+        // changed some parts of the code to be compatible in 3D, and point in the correct direction
         var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.down);
